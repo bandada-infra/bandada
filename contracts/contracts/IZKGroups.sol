135aca9925d7f379d8b9a2c85b08ddf09dd55ad4
jdhyun09 <jdhyun09@naver.com>
2022-11-01 11:30:14 +0900

    chore: add contract description

--- contracts/contracts/IZKGroups.sol
+++ contracts/contracts/IZKGroups.sol
@@ -1,15 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/*
-ZK-Groups will allow admins to also create on-chain groups.
-Admins will be able to create new on-chain groups with Metamask on Semaphore-supported networks.
-A ZK-Groups contract connected to the Semaphore.sol contract will be created,
-and it will also save the Merkle roots of the other off-chain groups
-(i.e. reputation and permissioned groups),
-which will then allow members of off-chain groups to verify their zero-knowledge proofs in that contract.
-*/
-
+/// @title zk-groups interface.
+/// @dev Interface of a ZKGroups contract.
 interface IZKGroups {
     struct Verifier {
         address contractAddress;
@@ -22,28 +15,51 @@ interface IZKGroups {
         uint256 merkleTreeDepth;
     }
 
+    /// @dev Emitted when an off-chain group is updated.
+    /// @param groupId: Id of the group.
+    /// @param name: Name of the off-chain group.
+    /// @param merkleTreeRoot: Root of the merkle tree.
+    /// @param merkleTreeDepth: Depth of the merkle tree.
     event OffchainGroupUpdated(
         uint256 groupId,
         bytes32 indexed name,
-        uint256 root,
+        uint256 merkleTreeRoot,
         uint256 indexed merkleTreeDepth
     );
 
+    /// @dev Emitted when a Semaphore proof is verified.
+    /// @param groupId: Id of the group.
+    /// @param externalNullifier: External nullifier.
+    /// @param nullifierHash: Nullifier hash.
+    /// @param signal: Semaphore signal.
     event ProofVerified(
         uint256 indexed groupId,
-        uint256 externalNullfier,
+        uint256 externalNullifier,
         uint256 nullifierHash,
         bytes32 signal
     );
 
+    /// @dev Emitted when an admin is assigned to a on-chain group.
+    /// @param groupId: Id of the group.
+    /// @param oldAdmin: Old admin of the on-chain group.
+    /// @param newAdmin: New admin of the on-chain group.
     event OnchainGroupAdminUpdated(
         uint256 indexed groupId,
         address indexed oldAdmin,
         address indexed newAdmin
     );
 
+    /// @dev Updates the off-chain groups.
+    /// @param groups: List of off-chain groups.
     function updateOffchainGroups(OffchainGroup[] calldata groups) external;
 
+    /// @dev Saves the nullifier hash to avoid double signaling and emits an event
+    /// if the zero-knowledge proof is valid. This is for off-chain group.
+    /// @param groupId: Id of the group.
+    /// @param signal: Semaphore signal.
+    /// @param nullifierHash: Nullifier hash.
+    /// @param externalNullifier: External nullifier.
+    /// @param proof: Zero-knowledge proof.
     function verifyOffchainGroupProof(
             uint256 groupId,
             bytes32 signal,
@@ -52,12 +68,24 @@ interface IZKGroups {
             uint256[8] calldata proof
         ) external;
 
+    /// @dev Returns the merkle tree root of an off-chain group.
+    /// @param groupId: Id of the group.
+    /// @return merkleTreeRoot: merkle tree root of the group.
     function getOffchainRoot(uint256 groupId) external view returns (uint256);
 
+    /// @dev Returns the merkle tree depth of an off-chain group.
+    /// @param groupId: Id of the group.
+    /// @return merkleTreeDepth: merkle tree Depth of the group.
     function getOffchainDepth(uint256 groupId) external view returns (uint256);
 
 
-
+    /// @dev Creates a new on-chain group. Only the admin will be able to add or remove members.
+    /// On-chain groups are managed in the deployed semaphore contract.
+    /// @param groupId: Id of the group.
+    /// @param merkleTreeDepth: merkle tree Depth of the group.
+    /// @param zeroValue: Zero value of the merkle tree.
+    /// @param admin: Admin of the group.
+    /// @param merkleTreeRootDuration: Time before the validity of a merkle tree root expires.
     function createOnchainGroup(
         uint256 groupId,
         uint256 merkleTreeDepth,
@@ -66,12 +94,30 @@ interface IZKGroups {
         uint256 merkleTreeRootDuration
     ) external;
 
+    /// @dev Updates the on-chain group admin.
+    /// @param groupId: Id of the group.
+    /// @param newAdmin: New admin of the group.
     function updateOnchainGroupAdmin(uint256 groupId, address newAdmin) external;
 
+    /// @dev Adds a new member to an existing on-chain group.
+    /// On-chain groups are managed in the deployed semaphore contract.
+    /// @param groupId: Id of the group.
+    /// @param identityCommitment: New identity commitment.
     function addMember(uint256 groupId, uint256 identityCommitment) external;
 
+    /// @dev Adds new members to an existing on-chain group.
+    /// @param groupId: Id of the group.
+    /// @param identityCommitments: New identity commitments.
     function addMembers(uint256 groupId, uint256[] calldata identityCommitments) external;
 
+    /// @dev Updates an identity commitment of an existing on-chain group. A proof of membership is
+    /// needed to check if the node to be updated is part of the merkle tree.
+    /// On-chain groups are managed in the deployed semaphore contract.
+    /// @param groupId: Id of the group.
+    /// @param identityCommitment: Existing identity commitment to be updated.
+    /// @param newIdentityCommitment: New identity commitment.
+    /// @param proofSiblings: Array of the sibling nodes of the proof of membership.
+    /// @param proofPathIndices: Path of the proof of membership.
     function updateMember(
         uint256 groupId,
         uint256 identityCommitment,
@@ -80,6 +126,13 @@ interface IZKGroups {
         uint8[] calldata proofPathIndices
     ) external;
 
+    /// @dev Removes a member from an existing on-chain group. A proof of membership is
+    /// needed to check if the node to be removed is part of the merkle tree.
+    /// On-chain groups are managed in the deployed semaphore contract.
+    /// @param groupId: Id of the group.
+    /// @param identityCommitment: Identity commitment to be removed.
+    /// @param proofSiblings: Array of the sibling nodes of the proof of membership.
+    /// @param proofPathIndices: Path of the proof of membership.
     function removeMember(
         uint256 groupId,
         uint256 identityCommitment,
@@ -87,6 +140,14 @@ interface IZKGroups {
         uint8[] calldata proofPathIndices
     ) external;
 
+    /// @dev Saves the nullifier hash to avoid double signaling and emits an event
+    /// if the zero-knowledge proof is valid. This is for on-chain group.
+    /// nullifier hash and event happens in the deployed semaphore contract.
+    /// @param groupId: Id of the group.
+    /// @param signal: Semaphore signal.
+    /// @param nullifierHash: Nullifier hash.
+    /// @param externalNullifier: External nullifier.
+    /// @param proof: Zero-knowledge proof.
     function verifyOnchainGroupProof(
         uint256 groupId,
         uint256 merkleTreeRoot,
--- contracts/contracts/ZKGroups.sol
+++ contracts/contracts/ZKGroups.sol
@@ -8,15 +8,31 @@ import "@semaphore-protocol/contracts/base/SemaphoreCore.sol";
 import "@semaphore-protocol/contracts/base/SemaphoreConstants.sol";
 import "@semaphore-protocol/contracts/interfaces/IVerifier.sol";
 
+/// @title zk-groups
+/// @dev Zk-groups is a semaphore group that implements both off-chain and on-chain.
+/// zk-groups allow admins to create on-chain groups. Admins will be able to create
+/// new on-chain groups with Metamask on Semaphore-supported networks. A Zk-groups contract
+/// connected to the Semaphore.sol contract which is deployed, and it will also save
+/// the Merkle tree roots of the other off-chain groups(i.e. reputation and permissioned groups),
+/// which will then allow members of off-chain groups to verify their zero-knowledge proofs in that contract.
 contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
-
+    /// @dev semaphore interface for load deployed semaphore contract.
     ISemaphore public semaphore;
 
+    /// @dev Gets a group id and returns the off-chain group parameters.
     mapping(uint256 => OffchainGroup) public offchainGroups;
+    
+    /// @dev Gets a merkle tree depth and returns its verifier address.
     mapping(uint256 => IVerifier) public verifiers;
+
+    /// @dev Gets a group id and returns boolean about nullifierHashes used.
     mapping(uint256 => mapping(uint256 => bool)) internal nullifierHashes;
+
+    /// @dev Gets a group id and returns the on-chain group admin.
     mapping(uint256 => address) public onchainGroupAdmin;
 
+    /// @dev Checks if there is a verifier for the given merkle tree depth.
+    /// @param merkleTreeDepth: Depth of the merkle tree.
     modifier onlySupportedMerkleTreeDepth(uint256 merkleTreeDepth) {
         if (address(verifiers[merkleTreeDepth]) == address(0)) {
             revert("MerkleTreeDepth is not supported");
@@ -24,6 +40,8 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         _;
     }
 
+    /// @dev Checks if the on-chain group admin is the transaction sender.
+    /// @param groupId: Id of the group.
     modifier onlyOnchainGroupAdmin(uint256 groupId) {
         if (onchainGroupAdmin[groupId] != _msgSender()) {
             revert("Caller is not the onchain group admin");
@@ -31,6 +49,9 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         _;
     }
 
+    /// @dev Initializes the Semaphore and the Semaphore verifiers used to verify the user's ZK proofs.
+    /// @param semaphoreAddress: deployed Semaphore contract address.
+    /// @param _verifiers: List of Semaphore verifiers (address and related Merkle tree depth).
     constructor(address semaphoreAddress, Verifier[] memory _verifiers) {
         semaphore = ISemaphore(semaphoreAddress);
 
@@ -43,6 +64,7 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         }
     }
 
+    /// @dev See {IZKGroups-updateOffchainGroups}.
     function updateOffchainGroups(OffchainGroup[] calldata _offchainGroups) external override onlyOwner {
         for (uint8 i = 0; i < _offchainGroups.length; ){
             uint256 groupId = uint256(keccak256(abi.encodePacked("Offchain_",_offchainGroups[i].name))) % SNARK_SCALAR_FIELD;
@@ -55,6 +77,7 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         }
     }
 
+    /// @dev See {IZKGroups-verifyOffchainGroupProof}.
     function verifyOffchainGroupProof(
         uint256 groupId,
         bytes32 signal,
@@ -79,14 +102,19 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         emit ProofVerified(groupId, nullifierHash, externalNullifier, signal);
     }
 
+    /// @dev See {IZKGroups-getOffchainRoot}.
     function getOffchainRoot(uint256 groupId) public view override returns (uint256) {
         return offchainGroups[groupId].merkleTreeRoot;
     }
 
+    /// @dev See {IZKGroups-getOffchainDepth}.
     function getOffchainDepth(uint256 groupId) public view override returns (uint256) {
         return offchainGroups[groupId].merkleTreeDepth;
     }
 
+    /// @dev Updates an off-chain group.
+    /// @param groupId: Id of the group.
+    /// @param offchainGroup: off-chain group data.
     function _updateOffchainGroup(
         uint256 groupId,
         OffchainGroup calldata offchainGroup
@@ -96,11 +124,14 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         emit OffchainGroupUpdated(groupId, offchainGroup.name, offchainGroup.merkleTreeRoot, offchainGroup.merkleTreeDepth);
     }
 
+    /// @dev Save nullifier hash to avoid double signaling.
+    /// @param groupId: Id of the group.
+    /// @param nullifierHash: Nullifier hash.
     function _saveNullifierHash(uint256 groupId, uint256 nullifierHash) internal {
         nullifierHashes[groupId][nullifierHash] = true;
     }
 
-
+    /// @dev See {IZKGroups-createOnchainGroup}.
     function createOnchainGroup(
         uint256 groupId,
         uint256 merkleTreeDepth,
@@ -114,20 +145,24 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         emit OnchainGroupAdminUpdated(groupId, address(0), admin);
     }
 
+    /// @dev See {IZKGroups-updateOnchainGroupAdmin}.
     function updateOnchainGroupAdmin(uint256 groupId, address newAdmin) external override onlyOnchainGroupAdmin(groupId) {
         onchainGroupAdmin[groupId] = newAdmin;
 
         emit OnchainGroupAdminUpdated(groupId, _msgSender(), newAdmin);
     }
 
+    /// @dev See {IZKGroups-addMember}.
     function addMember(uint256 groupId, uint256 identityCommitment) external override onlyOnchainGroupAdmin(groupId) {
         semaphore.addMember(groupId, identityCommitment);
     }
 
+    /// @dev See {IZKGroups-addMembers}.
     function addMembers(uint256 groupId, uint256[] calldata identityCommitments) external override onlyOnchainGroupAdmin(groupId) {
         semaphore.addMembers(groupId, identityCommitments);
     }
 
+    /// @dev See {IZKGroups-updateMember}.
     function updateMember(
         uint256 groupId,
         uint256 identityCommitment,
@@ -138,6 +173,7 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         semaphore.updateMember(groupId, identityCommitment, newIdentityCommitment, proofSiblings, proofPathIndices);
     }
 
+    /// @dev See {IZKGroups-removeMember}.
     function removeMember(
         uint256 groupId,
         uint256 identityCommitment,
@@ -147,6 +183,7 @@ contract ZKGroups is IZKGroups, SemaphoreCore, Ownable {
         semaphore.removeMember(groupId, identityCommitment, proofSiblings, proofPathIndices);
     }
 
+    /// @dev See {IZKGroups-verifyOnchainGroupProof}.
     function verifyOnchainGroupProof(
         uint256 groupId,
         uint256 merkleTreeRoot,
