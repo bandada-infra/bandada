29b0b67cc31cb6edee1ea66d720af4f8d9292e46
Saleel <saleel@me.com>
2023-01-27 12:32:06 +0530

    fix: fix formatting in about section of manage group page

--- apps/dashboard/src/pages/manage.tsx
+++ apps/dashboard/src/pages/manage.tsx
@@ -82,11 +82,18 @@ export default function Manage(): JSX.Element {
                     <Text fontWeight="bold" fontSize="lg">
                         Members
                     </Text>
-                    <Box borderBottom="1px" borderColor="gray.200" p="16px">
-                        <Text fontWeight="bold" fontSize="16px">
-                            Identity Commitment
+                    {_membersList?.length ? (
+                        <Box borderBottom="1px" borderColor="gray.200" p="16px">
+                            <Text fontWeight="bold" fontSize="16px">
+                                Identity Commitment
+                            </Text>
+                        </Box>
+                    ) : (
+                        <Text mt="5">
+                            No members in the group yet.
                         </Text>
-                    </Box>
+                    )}
+                    
                     {_membersList?.map((member) => (
                         <Flex
                             borderBottom="1px"
@@ -112,33 +119,30 @@ export default function Manage(): JSX.Element {
                         justifyContent="space-between"
                         mt="23px"
                     >
+                        {_group?.description && (
+                            <Box>
+                                <Text mb="10">{_group?.description}</Text>
+                            </Box>
+                        )}
                         <Box>
-                            <Text>{_group?.description}</Text>
-                        </Box>
-                        <Box>
-                            <Text>{_group?.members.length} members</Text>
-                            <Text mt="10">
+                            <Text mt="5">{_group?.members.length || 'No'} members</Text>
+                            <Text mt="5">
                                 {_group?.treeDepth === 30
-                                    ? "Capacity 1 Billion"
+                                    ? "Capacity: 1 Billion"
                                     : _group?.treeDepth === 25
-                                    ? "Capacity 30 Million"
+                                    ? "CapacityL 30 Million"
                                     : _group?.treeDepth === 20
-                                    ? "Capacity 500 Thousand"
-                                    : "Capacity 30 Thousand"}
+                                    ? "Capacity: 500 Thousand"
+                                    : "Capacity: 30 Thousand"}
                             </Text>
-                        </Box>
-                        <Box>
-                            <Text>
+                            <Text mt="5">
                                 {_group?.treeDepth === 30
-                                    ? "Tree depth 30"
+                                    ? "Tree depth: 30"
                                     : _group?.treeDepth === 25
-                                    ? "Tree depth 25"
+                                    ? "Tree depth: 25"
                                     : _group?.treeDepth === 20
-                                    ? "Tree depth 20"
-                                    : "Tree depth 16"}
-                            </Text>
-                            <Text mt="10">
-                                What is treedepth? Merkle trees are used to....
+                                    ? "Tree depth: 20"
+                                    : "Tree depth: 16"}
                             </Text>
                         </Box>
                     </Flex>
