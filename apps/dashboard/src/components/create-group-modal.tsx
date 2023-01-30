6de192b407d0984c2e5a9ade7e73a25f172198e1
Saleel <saleel@me.com>
2023-01-30 22:11:29 +0530

    feat: auto-refresh group list on creating new group

--- apps/dashboard/src/components/create-group-modal.tsx
+++ apps/dashboard/src/components/create-group-modal.tsx
@@ -15,7 +15,6 @@ import {
     ModalOverlay,
     Spinner,
     Text,
-    UseDisclosureProps
 } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { groupSizeInfo } from "../types/groups"
@@ -27,7 +26,7 @@ import useSigner from "../hooks/useSigner"
 export default function CreateGroupModal({
     isOpen,
     onClose
-}: UseDisclosureProps): JSX.Element {
+}: { isOpen: boolean, onClose: (created: boolean) => void}): JSX.Element {
     const [searchParams] = useSearchParams()
     const pageOption = searchParams.get("type")
     const [_step, setStep] = useState<number>(0)
@@ -67,13 +66,13 @@ export default function CreateGroupModal({
                     _signer &&
                     (await semaphore.createGroup(_signer, groupName, treeDepth))
                 setLoading(false)
-                transaction && onClose && onClose()
+                transaction && onClose && onClose(true)
             } catch (error) {
                 console.error(error)
             }
         } else {
             createOffchainGroup(groupName, groupDescription, treeDepth)
-            onClose && onClose()
+            onClose && onClose(true)
         }
     }
 
@@ -123,7 +122,7 @@ export default function CreateGroupModal({
     }, [isOpen])
 
     return (
-        <Modal isOpen={!!isOpen} onClose={onClose ? onClose : console.error}>
+        <Modal isOpen={!!isOpen} onClose={() => onClose(false)}>
             <ModalOverlay />
             <ModalContent maxW={_step === 1 ? "1200px" : "600px"}>
                 <ModalHeader borderBottom="1px" borderColor="gray.200">
