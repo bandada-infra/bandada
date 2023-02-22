564b677740224287950dc03970c7cac84934d481
cedoor <me@cedoor.dev>
2023-02-22 21:21:06 +0100

    chore: update jest configuration files

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,33 @@
-import { getJestProjects } from "@nrwl/jest"
+import fs from "fs"
+import type { Config } from "@jest/types"
 
-export default {
-    projects: getJestProjects()
-}
+const libs: any = fs
+    .readdirSync("./libs", { withFileTypes: true })
+    .filter((directory) => directory.isDirectory())
+    .map(({ name }) => ({
+        rootDir: `libs/${name}`,
+        displayName: name,
+        setupFiles: ["dotenv/config"],
+        moduleNameMapper: {
+            "@zk-groups/(.*)": "<rootDir>/../$1/src/index.ts" // Interdependency packages.
+        }
+    }))
+
+export default async (): Promise<Config.InitialOptions> => ({
+    projects: libs,
+    verbose: true,
+    coverageDirectory: "./coverage/libs",
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.ts",
+        "!<rootDir>/src/**/index.ts",
+        "!<rootDir>/src/**/*.d.ts"
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 95,
+            lines: 95,
+            statements: 95
+        }
+    }
+})
