e466822638ef46f0811603f4a829ce6c3a2e1e2f
Nick Tampakas <nick@gmx.co.uk>
2023-03-16 14:37:32 +0200

    Change AWS timeout / Add new docker-compose for prod

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -4,6 +4,6 @@ set -e
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
 cd ~/zk-groups
-docker compose up -d --no-build
+docker compose -f docker-compose.prod.yml up -d --no-build
 
 exit 0
