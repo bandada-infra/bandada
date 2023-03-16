bf911f86c3745ec42fd3de17488eb9da63de11a8
Nick Tampakas <nick@gmx.co.uk>
2023-03-16 15:06:40 +0200

    Fix typo

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-pwd
-egrep build.*true ~/.killswitch || exit 0
+egrep build.*true .killswitch || exit 0
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
