1b8dee14f795fbefb9662be26e195d8f2f70297d
Saleel <saleel@me.com>
2023-03-24 18:14:37 +0530

    chore: add pull policy to docker compose

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -4,7 +4,7 @@ set -e
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
 cd ~/bandada
-docker compose -f docker-compose.prod.yml pull
+
 docker compose -f docker-compose.prod.yml up -d --no-build
 
 exit 0
