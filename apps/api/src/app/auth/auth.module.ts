7ba9dfc368083240e0efc10e6640adbf972720d6
Saleel <saleel@saleel.xyz>
2023-04-17 14:00:55 +0530

    chore: remove oAuth libs

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,33 +1,20 @@
 import { Module } from "@nestjs/common"
 import { JwtModule } from "@nestjs/jwt"
-import { PassportModule } from "@nestjs/passport"
 import { UsersModule } from "../users/users.module"
 import { CookieSerializer } from "../utils"
 import { AuthController } from "./auth.controller"
 import { AuthService } from "./auth.service"
-import { GithubStrategy } from "./strategies/github.strategy"
-import { JwtStrategy } from "./strategies/jwt.strategy"
-import { RedditStrategy } from "./strategies/reddit.strategy"
-import { TwitterStrategy } from "./strategies/twitter.strategy"
 
 @Module({
     imports: [
         UsersModule,
-        PassportModule.register({ session: true }),
         JwtModule.register({
             secret: process.env.JWT_SECRET_KEY,
             signOptions: { expiresIn: "300s" }
         })
     ],
-    providers: [
-        GithubStrategy,
-        TwitterStrategy,
-        RedditStrategy,
-        AuthService,
-        CookieSerializer,
-        JwtStrategy
-    ],
+    providers: [AuthService, CookieSerializer],
     controllers: [AuthController],
-    exports: [PassportModule, JwtModule]
+    exports: [JwtModule]
 })
 export class AuthModule {}
