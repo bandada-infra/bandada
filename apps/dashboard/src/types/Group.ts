9b904a6f0ed8a1dc8b7dac9cf9ebb0248478ca94
cedoor <me@cedoor.dev>
2023-05-18 19:05:20 +0100

    refactor(dashboard): udpate types

---
+++ apps/dashboard/src/types/Group.ts
@@ -0,0 +1,22 @@
+export type Group = {
+    id: string
+    name: string
+    type?: string
+    description: string
+    treeDepth: number
+    members: string[]
+    admin: string
+    apiEnabled?: boolean
+    apiKey?: string
+}
+
+export type GroupSize = {
+    description: string
+    capacity: string
+    useCases: string[]
+    treeDepth: number
+}
+
+export type GroupSizes = Record<string, GroupSize>
+
+export default Group
