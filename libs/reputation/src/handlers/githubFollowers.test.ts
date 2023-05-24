61fb9d2f500bf971cfa8790d75171d2d5bb6c83c
cedoor <me@cedoor.dev>
2023-05-24 16:40:38 +0100

    test(reputation): mock fetch function for handler tests
    re #201

--- libs/reputation/src/handlers/githubFollowers.test.ts
+++ libs/reputation/src/handlers/githubFollowers.test.ts
@@ -1,15 +1,24 @@
 import validateReputation from "../validateReputation"
 
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+        json: () =>
+            Promise.resolve({
+                followers: 110
+            })
+    })
+) as any
+
 describe("GithubFollowers", () => {
     it("Should return true if a Github user has more than 100 followers", async () => {
-        const result = validateReputation(
+        const result = await validateReputation(
             {
                 type: "GITHUB_FOLLOWERS",
                 parameters: {
                     minFollowers: 100
                 }
             },
-            { githubAccessToken: "" }
+            { githubAccessToken: "token" }
         )
 
         expect(result).toBeTruthy()
