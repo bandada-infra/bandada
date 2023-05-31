76310b543098724334bb8fa4cd76c36fd7abe6c9
cedoor <me@cedoor.dev>
2023-05-31 11:41:50 +0100

    feat(reputation): add new github validator
    re #207

--- libs/reputation/src/validators/index.ts
+++ libs/reputation/src/validators/index.ts
@@ -1,5 +1,11 @@
 import githubFollowers from "./githubFollowers"
+import githubPersonalStars from "./githubPersonalStars"
 import githubRepositoryCommits from "./githubRepositoryCommits"
 import twitterFollowers from "./twitterFollowers"
 
-export { githubFollowers, githubRepositoryCommits, twitterFollowers }
+export {
+    githubFollowers,
+    githubRepositoryCommits,
+    twitterFollowers,
+    githubPersonalStars
+}
