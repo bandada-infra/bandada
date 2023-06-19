c07f1cc78d5118ddf0f3d13ea86455b9722bfc18
cedoor <me@cedoor.dev>
2023-06-19 13:53:45 +0100

    fix(reputation): add query parameter to get right response
    re #202

--- libs/reputation/src/validators/twitterFollowers/index.ts
+++ libs/reputation/src/validators/twitterFollowers/index.ts
@@ -19,7 +19,9 @@ const criteriaABI = {
 const validate: Handler = async (criteria: Criteria, { utils }) => {
     utils.checkCriteria(criteria, criteriaABI)
 
-    const { data } = await utils.twitterAPI("users/me")
+    const { data } = await utils.twitterAPI(
+        "users/me?user.fields=public_metrics"
+    )
 
     return data.public_metrics.followers_count >= criteria.minFollowers
 }
