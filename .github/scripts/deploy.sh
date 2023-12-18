65fa93de688fe08b6a3f9e8b60ccf6be08566a22
Nick Tampakas <nick@gmx.co.uk>
2023-12-18 12:48:20 +0200

    Modify aux scripts needed for build + deploy

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -ex
+
+env=""
+
+[ "$1" = "prod" ] || env="-$1"
  
-tasks="bandada-client bandada-api bandada-dashboard"
+tasks="bandada-client$env bandada-api$env bandada-dashboard$env"
 for task in $tasks; do
   bandada_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
-  aws ecs update-service --cluster bandada --service $task --force-new-deployment --task-definition $task:$bandada_revision
+  aws ecs update-service --cluster bandada$env --service $task --force-new-deployment --task-definition $task:$bandada_revision
 done
 
-for loop in {1..2}; do
-  aws ecs wait services-stable --cluster bandada --services $tasks && break || continue
+for loop in {1..3}; do
+  [ "$loop" -eq 3 ] && exit 1
+  aws ecs wait services-stable --cluster bandada$env --services $tasks && break || continue
 done
-
-exit 0
