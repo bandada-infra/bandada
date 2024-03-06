d99f751dabc55318d28829c5051a1da6ef58c1e5
Vivian Plasencia <v.pcalana@gmail.com>
2024-03-06 23:43:39 +0100

    style(docs): format code with prettier

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -3,128 +3,128 @@ import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
 
 const config: Config = {
-  title: "Bandada docs",
-  tagline: "Bandada documentation",
-  favicon: "img/favicon.ico",
+    title: "Bandada docs",
+    tagline: "Bandada documentation",
+    favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+    // Set the production url of your site here
+    url: "https://your-docusaurus-site.example.com",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
+    onBrokenLinks: "warn",
+    onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"]
-  },
-
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          routeBasePath: "/",
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        },
-        blog: false,
-        theme: {
-          customCss: "./src/css/custom.css"
-        }
-      } satisfies Preset.Options
-    ]
-  ],
-
-  themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "bandada",
-      logo: {
-        alt: "Bandada Logo",
-        src: "img/logo.svg"
-      },
-      items: [
-        {
-          href: "https://bandada.pse.dev",
-          label: "App",
-          position: "right"
-        },
-        {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right"
-        }
-      ]
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"]
     },
-    footer: {
-      style: "dark",
-      logo: {
-        src: "img/logo-text.svg",
-        width: 160,
-        height: 51
-      },
-      links: [
-        {
-          title: "Community",
-          items: [
+
+    presets: [
+        [
+            "classic",
             {
-              label: "PSE Discord",
-              href: "https://discord.com/invite/sF5CT5rzrR"
+                docs: {
+                    routeBasePath: "/",
+                    sidebarPath: "./sidebars.ts",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+                },
+                blog: false,
+                theme: {
+                    customCss: "./src/css/custom.css"
+                }
+            } satisfies Preset.Options
+        ]
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "bandada",
+            logo: {
+                alt: "Bandada Logo",
+                src: "img/logo.svg"
             },
-            {
-              label: "GitHub",
-              href: "https://github.com/privacy-scaling-explorations/bandada"
-            },
-            {
-              label: "X (Twitter)",
-              href: "https://twitter.com/BandadaDevs"
-            }
-          ]
+            items: [
+                {
+                    href: "https://bandada.pse.dev",
+                    label: "App",
+                    position: "right"
+                },
+                {
+                    href: "https://github.com/facebook/docusaurus",
+                    label: "GitHub",
+                    position: "right"
+                }
+            ]
         },
-        {
-          title: "Build",
-          items: [
-            {
-              label: "App",
-              href: "https://bandada.pse.dev"
+        footer: {
+            style: "dark",
+            logo: {
+                src: "img/logo-text.svg",
+                width: 160,
+                height: 51
             },
-            {
-              label: "API",
-              href: "https://api.bandada.pse.dev"
-            },
-            {
-              label: "Boilerplate",
-              href: "https://github.com/vplasencia/bandada-semaphore-demo"
-            },
-            {
-              label: "API SDK demo",
-              href: "https://github.com/vplasencia/bandada-sdk-demo"
-            }
-          ]
+            links: [
+                {
+                    title: "Community",
+                    items: [
+                        {
+                            label: "PSE Discord",
+                            href: "https://discord.com/invite/sF5CT5rzrR"
+                        },
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/privacy-scaling-explorations/bandada"
+                        },
+                        {
+                            label: "X (Twitter)",
+                            href: "https://twitter.com/BandadaDevs"
+                        }
+                    ]
+                },
+                {
+                    title: "Build",
+                    items: [
+                        {
+                            label: "App",
+                            href: "https://bandada.pse.dev"
+                        },
+                        {
+                            label: "API",
+                            href: "https://api.bandada.pse.dev"
+                        },
+                        {
+                            label: "Boilerplate",
+                            href: "https://github.com/vplasencia/bandada-semaphore-demo"
+                        },
+                        {
+                            label: "API SDK demo",
+                            href: "https://github.com/vplasencia/bandada-sdk-demo"
+                        }
+                    ]
+                }
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Ethereum Foundation`
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula
         }
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Ethereum Foundation`
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula
-    }
-  } satisfies Preset.ThemeConfig
+    } satisfies Preset.ThemeConfig
 }
 
 export default config
--- apps/docs/src/components/RenderVideos/index.tsx
+++ apps/docs/src/components/RenderVideos/index.tsx
@@ -1,26 +1,27 @@
 import videos from "../../../static/data/videos.json"
 
 export default function RenderVideos(): JSX.Element {
-  return (
-    <div>
-      {videos
-        .sort(
-          (a, b) =>
-            new Date(b.date || "").getTime() - new Date(a.date || "").getTime()
-        )
-        .map((video) => (
-          <div style={{ margin: "15px" }}>
-            {" "}
-            <a
-              href={video.url}
-              target="_blank"
-              rel="noreferrer noopener nofollow"
-            >
-              {video.title}
-            </a>{" "}
-            - {video.speakers.join(", ")}{" "}
-          </div>
-        ))}
-    </div>
-  )
+    return (
+        <div>
+            {videos
+                .sort(
+                    (a, b) =>
+                        new Date(b.date || "").getTime() -
+                        new Date(a.date || "").getTime()
+                )
+                .map((video) => (
+                    <div style={{ margin: "15px" }}>
+                        {" "}
+                        <a
+                            href={video.url}
+                            target="_blank"
+                            rel="noreferrer noopener nofollow"
+                        >
+                            {video.title}
+                        </a>{" "}
+                        - {video.speakers.join(", ")}{" "}
+                    </div>
+                ))}
+        </div>
+    )
 }
