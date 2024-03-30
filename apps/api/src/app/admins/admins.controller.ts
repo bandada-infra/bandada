fa7ceccd4ac06f1bed426789022a7779202a7653
Vivian Plasencia <v.pcalana@gmail.com>
2024-03-30 13:36:59 +0100

    fix: remove the creation of an admin in the dashboard app
    The admin must be created in the Bandada backend.
    
    re #460

--- apps/api/src/app/admins/admins.controller.ts
+++ apps/api/src/app/admins/admins.controller.ts
@@ -1,29 +1,12 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Param,
-    Post,
-    Put,
-    UseGuards
-} from "@nestjs/common"
+import { Body, Controller, Get, Param, Put, UseGuards } from "@nestjs/common"
 import { ApiExcludeEndpoint } from "@nestjs/swagger"
 import { AuthGuard } from "../auth/auth.guard"
-import { CreateAdminDTO } from "./dto/create-admin.dto"
 import { AdminsService } from "./admins.service"
-import { Admin } from "./entities/admin.entity"
 import { UpdateApiKeyDTO } from "./dto/update-apikey.dto"
 
 @Controller("admins")
 export class AdminsController {
     constructor(private readonly adminsService: AdminsService) {}
-
-    @Post()
-    @UseGuards(AuthGuard)
-    @ApiExcludeEndpoint()
-    async createAdmin(@Body() dto: CreateAdminDTO): Promise<Admin> {
-        return this.adminsService.create(dto)
-    }
 
     @Get(":admin")
     @UseGuards(AuthGuard)
