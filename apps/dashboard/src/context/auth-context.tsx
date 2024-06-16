aaa2f3c4cfe47c5545501e0292eff516a0e32138
ArmandoCode <totovip1464@gmail.com>
2024-06-16 17:26:26 -0600

    refactor: update AuthContextProvider to support wagmi and rainbowkit v2 API changes

--- apps/dashboard/src/context/auth-context.tsx
+++ apps/dashboard/src/context/auth-context.tsx
@@ -1,131 +1,99 @@
 import {
     AuthenticationStatus,
-    connectorsForWallets,
     createAuthenticationAdapter,
     lightTheme,
     RainbowKitAuthenticationProvider,
     RainbowKitProvider
-} from "@rainbow-me/rainbowkit"
-import {
-    coinbaseWallet,
-    injectedWallet,
-    metaMaskWallet,
-    walletConnectWallet
-} from "@rainbow-me/rainbowkit/wallets"
-import React, { ReactNode, useEffect, useMemo, useState } from "react"
-import { SiweMessage } from "siwe"
-import { configureChains, createClient, WagmiConfig } from "wagmi"
-import { sepolia } from "wagmi/chains"
-import { publicProvider } from "wagmi/providers/public"
-import { getNonce, logOut, signIn } from "../api/bandadaAPI"
-import useSessionData from "../hooks/use-session-data"
-import { Admin } from "../types"
-
-const appName = "Bandada"
-
-export const AuthContext = React.createContext<{ admin?: Admin | null }>({})
-
-export function AuthContextProvider({ children }: { children: ReactNode }) {
+  } from "@rainbow-me/rainbowkit";
+  import React, { ReactNode, useEffect, useMemo, useState } from "react";
+  import { WagmiProvider } from "wagmi";
+  import { getNonce, logOut, signIn } from "../api/bandadaAPI";
+  import useSessionData from "../hooks/use-session-data";
+  import { Admin } from "../types";
+  import { SiweMessage } from 'siwe';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { configWallets } from "../lib/configWallets";
+  
+  const appName = "Bandada";
+  
+  export const AuthContext = React.createContext<{ admin?: Admin | null }>({});
+  
+  export function AuthContextProvider({ children }: { children: ReactNode }) {
     const [authStatus, setAuthStatus] =
-        useState<AuthenticationStatus>("loading")
-    const { admin, saveAdmin, deleteAdmin } = useSessionData()
-
+      useState<AuthenticationStatus>("loading");
+    const { admin, saveAdmin, deleteAdmin } = useSessionData();
+  
     useEffect(() => {
-        setAuthStatus(admin ? "authenticated" : "unauthenticated")
-    }, [admin])
-
-    const contextValue = useMemo(() => ({ admin }), [admin])
+      setAuthStatus(admin ? "authenticated" : "unauthenticated");
+    }, [admin]);
+  
+    const contextValue = useMemo(() => ({ admin }), [admin]);
     const authAdapter = useMemo(
-        () =>
-            createAuthenticationAdapter({
-                async getNonce() {
-                    const nonce = await getNonce()
-
-                    return nonce ?? ""
-                },
-
-                createMessage: ({ nonce, address, chainId }) =>
-                    new SiweMessage({
-                        domain: window.location.host,
-                        address,
-                        statement:
-                            "You are using your Ethereum Wallet to sign in to Bandada.",
-                        uri: window.location.origin,
-                        version: "1",
-                        chainId,
-                        nonce
-                    }),
-
-                getMessageBody: ({ message }) => message.prepareMessage(),
-
-                verify: async ({ message, signature }) => {
-                    const admin: Admin = await signIn({
-                        message,
-                        signature
-                    })
-
-                    if (admin) {
-                        saveAdmin(admin)
-
-                        return true
-                    }
-
-                    return false
-                },
-
-                signOut: async () => {
-                    await logOut()
-
-                    deleteAdmin()
-                }
+      () =>
+        createAuthenticationAdapter({
+          async getNonce() {
+            const nonce = await getNonce();
+            return nonce ?? "";
+          },
+  
+          createMessage: ({ nonce, address, chainId }) =>
+            new SiweMessage({
+              domain: window.location.host,
+              address,
+              statement:
+                "You are using your Ethereum Wallet to sign in to Bandada.",
+              uri: window.location.origin,
+              version: "1",
+              chainId,
+              nonce
             }),
-        [saveAdmin, deleteAdmin]
-    )
-
-    const { chains, provider, webSocketProvider } = configureChains(
-        [sepolia],
-        [publicProvider()]
-    )
-
-    const connectors = connectorsForWallets([
-        {
-            groupName: "Wallets",
-            wallets: [
-                injectedWallet({ chains }),
-                metaMaskWallet({ chains }),
-                coinbaseWallet({ appName, chains }),
-                walletConnectWallet({ chains })
-            ]
-        }
-    ])
-
-    const wagmiClient = createClient({
-        autoConnect: true,
-        connectors,
-        provider,
-        webSocketProvider
-    })
-
+  
+          getMessageBody: ({ message }) => message.prepareMessage(),
+  
+          verify: async ({ message, signature }) => {
+            const admin: Admin = await signIn({
+              message,
+              signature
+            });
+  
+            if (admin) {
+              saveAdmin(admin);
+              return true;
+            }
+  
+            return false;
+          },
+  
+          signOut: async () => {
+            await logOut();
+            deleteAdmin();
+          }
+        }),
+      [saveAdmin, deleteAdmin]
+    );
+  
+    const queryClient = new QueryClient()
+  
     return (
         <AuthContext.Provider value={contextValue}>
-            <WagmiConfig client={wagmiClient}>
-                <RainbowKitAuthenticationProvider
-                    adapter={authAdapter}
-                    status={authStatus}
+          <WagmiProvider config={configWallets}>
+            <QueryClientProvider client={queryClient}>
+              <RainbowKitAuthenticationProvider
+                adapter={authAdapter}
+                status={authStatus}
+              >
+                <RainbowKitProvider
+                  modalSize="compact"
+                  theme={lightTheme()}
+                  appInfo={{ appName }}
+                  showRecentTransactions={false}
                 >
-                    <RainbowKitProvider
-                        chains={chains}
-                        modalSize="compact"
-                        theme={lightTheme()}
-                        appInfo={{
-                            appName
-                        }}
-                        showRecentTransactions={false}
-                    >
-                        {children}
-                    </RainbowKitProvider>
-                </RainbowKitAuthenticationProvider>
-            </WagmiConfig>
+                  {children}
+                </RainbowKitProvider>
+              </RainbowKitAuthenticationProvider>
+            </QueryClientProvider >
+          </WagmiProvider>
         </AuthContext.Provider>
-    )
-}
+    );
+  }
+  
