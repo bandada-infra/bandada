95af97691e487ff05ef98ec317aab4894d7d8986
Vivian Plasencia <v.pcalana@gmail.com>
2024-08-07 20:44:56 +0200

    chore(credentials): use the zk-kit logical-expressions package
    The logic to evaluate logical expressions in Bandada was moved to a new package called
    @zk-kit/logical-expressions.
    
    re #543

--- libs/credentials/src/index.test.ts
+++ libs/credentials/src/index.test.ts
@@ -16,7 +16,6 @@ import {
     validateManyCredentials
 } from "./validateCredentials"
 import { testUtils } from "."
-import { evaluate, tokenize } from "./evaluateExpression"
 import checkCriteria from "./checkCriteria"
 import getJsonRpcProvider from "./getJsonRpcProvider"
 import queryGraph from "./queryGraph"
@@ -313,93 +312,6 @@ describe("Credentials library", () => {
             )
 
             expect(result).toBeTruthy()
-        })
-    })
-    describe("# evaluateExpression", () => {
-        describe("# tokenize", () => {
-            it("Should sucessfully tokenize a logical expression", () => {
-                const expression = "true and false or ( true and true )"
-
-                const tokens = tokenize(expression)
-
-                const result = [
-                    "true",
-                    "and",
-                    "false",
-                    "or",
-                    "(",
-                    "true",
-                    "and",
-                    "true",
-                    ")"
-                ]
-
-                expect(tokens).toStrictEqual(result)
-            })
-        })
-        describe("# evaluate", () => {
-            it("Should sucessfully evaluate a logical expression with the and operator", () => {
-                const expression = ["true", "and", "false"]
-                const result = evaluate(expression)
-                expect(result).toBeFalsy()
-            })
-            it("Should sucessfully evaluate a logical expression with the or operator", () => {
-                const expression = ["true", "or", "false"]
-                const result = evaluate(expression)
-                expect(result).toBeTruthy()
-            })
-            it("Should sucessfully evaluate a logical expression with the not operator", () => {
-                const expression = ["not", "false"]
-                const result = evaluate(expression)
-                expect(result).toBeTruthy()
-            })
-            it("Should sucessfully evaluate a logical expression with the xor operator", () => {
-                const expression = ["false", "xor", "true"]
-                const result = evaluate(expression)
-                expect(result).toBeTruthy()
-            })
-            it("Should sucessfully evaluate a logical expression with the and or not and xor operators", () => {
-                const expression = [
-                    "true",
-                    "and",
-                    "false",
-                    "or",
-                    "not",
-                    "false",
-                    "xor",
-                    "true"
-                ]
-                const result = evaluate(expression)
-                expect(result).toBeFalsy()
-            })
-            it("Should sucessfully evaluate a logical expression with parentheses", () => {
-                const expression = [
-                    "true",
-                    "and",
-                    "false",
-                    "or",
-                    "(",
-                    "true",
-                    "and",
-                    "true",
-                    ")"
-                ]
-                const result = evaluate(expression)
-                expect(result).toBeTruthy()
-            })
-            it("Should sucessfully evaluate a logical expression with the and or not and xor operators in parentheses", () => {
-                const expression = [
-                    "true",
-                    "and",
-                    "false",
-                    "or",
-                    "(",
-                    "not",
-                    ")"
-                ]
-                const result = evaluate(expression)
-                expect(result).toBeTruthy()
-            })
         })
     })
 })
