2eb31eb747d45e8366ed95636a2917a726f9104c
Han <56923450+waddaboo@users.noreply.github.com>
2024-10-10 17:35:22 +0800

    feat(utils): add get easCredentialSupportedNetworks util

---
+++ libs/utils/src/getEasSupportedNetwork.ts
@@ -0,0 +1,52 @@
+import type { BlockchainNetwork } from "./types"
+
+export const easCredentialSupportedNetworks: BlockchainNetwork[] = [
+    {
+        id: "ethereum",
+        name: "Ethereum"
+    },
+    {
+        id: "sepolia",
+        name: "Ethereum (Sepolia)"
+    },
+    {
+        id: "arbitrum",
+        name: "Arbitrum"
+    },
+    {
+        id: "arbitrum-nova",
+        name: "Arbitrum Nova"
+    },
+    {
+        id: "base",
+        name: "base"
+    },
+    {
+        id: "base-sepolia",
+        name: "Base (Sepolia)"
+    },
+    {
+        id: "optimism",
+        name: "Optimism"
+    },
+    {
+        id: "optimism-sepolia-bedrock",
+        name: "Optimism (Sepolia)"
+    },
+    {
+        id: "scroll",
+        name: "scroll"
+    },
+    {
+        id: "polygon",
+        name: "Polygon"
+    },
+    {
+        id: "linea",
+        name: "Linea"
+    },
+    {
+        id: "celo",
+        name: "Celo"
+    }
+]
